ef983dd0ac24d42271349818c9bbbfa91aec84d2
Aldungarov Artem <shepardcmndr@gmail.com>
2022-12-17 19:18:56 +0400

    homework 9 done

--- src/main/java/practice/regex/SplitText.java
+++ src/main/java/practice/regex/SplitText.java
@@ -7,8 +7,21 @@ public class SplitText {
   }
 
   public static String splitTextIntoWords(String text) {
-    //TODO реализуйте метод
-    return "";
+    if (text.isBlank()) {
+      return "";
+    }
+
+    String regex = "[^a-zA-Z]";
+    String[] split = text.split(regex);
+    StringBuilder stringBuilder = new StringBuilder();
+
+    for (String word : split) {
+      if (!word.isBlank()) {
+        stringBuilder.append(word.strip()).append(System.lineSeparator());
+      }
+    }
+
+    return stringBuilder.toString().strip();
   }
 
 }
